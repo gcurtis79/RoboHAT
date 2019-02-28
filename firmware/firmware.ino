2edebbab154b26ec1de8db711ddc254d81df5866
Greg Curtis <gcurtis79@gmail.com>
2019-02-28 01:37:48 -0600

    Adjustments

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -3,9 +3,6 @@
 #include <Wire.h>
 #include <EveryTimer.h>
 #include <TimeLib.h>
-#include <U8g2lib.h>
-
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
 MPU6050 mpu6050(Wire);
 
@@ -15,11 +12,11 @@ bool LED = true;
 int LED_PIN = 7;
 
 void flash(int,int);
+void dd();
 
 void setup() {
   Serial.begin(9600);
   delay(1000);
-  u8g2.begin();  
   Wire.begin();
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
@@ -33,17 +30,7 @@ void loop() {
   gyroDisp.Update();
 }
 
-char lines[8][1];
-
-void dd(int line, char* a, char* n = "") {
-  char spaces[30] = "                             ";
-  sprintf(lines[line], "%s%s", a, n);
-  strcat(lines[line], &spaces[strlen(lines[line])]);
-  u8g2.setCursor(0, (line * 8));
-  u8g2.print(lines[line]);
-  u8g2.sendBuffer();
-}
-{
+void dd() {
   Serial.print("angleX : ");
   Serial.print(mpu6050.getAngleX());
   Serial.print("\tangleY : ");
